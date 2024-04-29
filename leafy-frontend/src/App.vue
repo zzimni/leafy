0dffede0cc225e45d5fbffcb9e4e65245258e1c3
zzimni <hjm3327@naver.com>
2024-04-29 10:58:37 +0900

    github action test

--- leafy-frontend/src/App.vue
+++ leafy-frontend/src/App.vue
@@ -3,6 +3,7 @@
     <div class="welcome-message" v-if="isLoggedIn">
       <p class="welcome-text">안녕하세요, <span class="user-name">{{ user.name }}</span>님!</p>
       <p class="description">오늘도 즐거운 식물 관리하세요.</p>
+      <p class="description">Github action test</p>
     </div>
     <div class="brand" v-if="isLoggedIn">
       LEAFY
